ca08e22ee210112627b6ebc7caa90bfdf15e3fc1
Joss Doebler <mail@joss-doebler.de>
2021-07-08 11:46:06 +0200

    feat: add codeine as dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,13 @@
 import type { AppProps } from 'next/app'
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import codeine from '@jossdoe/codeine';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-};
+const { GlobalStyles } = codeine.global;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <GlobalStyles />
+      <Component {...pageProps} />
     </>
   );
 }
